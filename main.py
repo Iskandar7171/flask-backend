c852340782893cd92ad1ae4a3ce429e0733dc7df
Iskandar7171 <tolagankhojaeviskandar@gmail.com>
2025-10-17 14:47:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask("__name__")
 @app.route("/")
 def index():
     return "Salom beckend dasturchi!"
+@app.route("/about")
+def home():
+    return "about page"
 
 @app.route("/home")
 def home():
